56c640775e0fa9ddb22d2748c700fe9247d7f703
CemYilmaz <cemyilmaz463@gmail.com>
2023-02-10 19:45:23 +0300

    genreEnum added

---
+++ BookStore2/Common/GenreEnum.cs
@@ -0,0 +1,22 @@
+namespace BookStore2.Common
+{
+    public enum GenreEnum
+    {
+        Fiction = 1,
+        NonFiction = 2,
+        Biography = 3,
+        History = 4,
+        Science = 5,
+        Technology = 6,
+        Travel = 7,
+        Religion = 8,
+        SelfHelp = 9,
+        Health = 10,
+        Children = 11,
+        Cooking = 12,
+        Art = 13,
+        Music = 14,
+        Sports = 15,
+        Other = 16
+    }
+}
